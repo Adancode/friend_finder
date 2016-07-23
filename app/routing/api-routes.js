8f75f91db7af6ebca6258005d1307c45412b14fb
Adam Camacho <adamcamacho8@gmail.com>
2016-07-23 03:15:45 -0500

    corrected a link to the friends api

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,13 +3,12 @@ var friendData = require('../data/friends.js');
 var bodyParser = require('body-parser');
 
 module.exports = function(app) {
-	app.get("/api/friends", function(req,res) {
 
+	app.get("/api/friends", function(req,res) {
 		res.json(friendData);
 	});
 
      app.post("/api/friends", function (req, res){
-          //console.log(friendData);
           friendData.push(req.body);
           res.json(friendData);
      });
@@ -17,7 +16,6 @@ module.exports = function(app) {
      /*app.post('/api/clear', function(req, res){
 		// Empty out the arrays of data
 		friendData = [];
-
 		console.log(friendData);
 	});*/
 
